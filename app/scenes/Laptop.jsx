1035fe74d9f583767fecd05b70fdf324295ee9b6
Dimas hilmawan <hilmawan22@gmail.com>
2023-08-29 18:22:19 +0700

    fix animation 2

--- app/scenes/Laptop.jsx
+++ app/scenes/Laptop.jsx
@@ -12,7 +12,7 @@ const Laptop = () => {
       <Container className="py-64 xl:py-52 lg:py-44 md:py-36 sm:py-28 sm:text-center xs:py-20">
         <div className="flex gap-12 lg:gap-8 sm:flex-col sm:gap-16 xs:gap-12">
           <div className="flex flex-1 items-center justify-center ">
-            <Image
+            {/* <Image
               alt="illustration laptop desktop"
               src={illustrationLaptopDesk}
               className="h-auto w-[300px] -translate-x-24 scale-[2.75] xl:-translate-x-28 xl:scale-[2.8] lg:-translate-x-36 lg:scale-[2.5] sm:hidden"
@@ -21,9 +21,9 @@ const Laptop = () => {
               alt="illustration laptop desktop"
               src={illustrationLaptopMobile}
               className="hidden h-auto w-full sm:block "
-            />
+            /> */}
           </div>
-          <div className="flex-1 ">
+          <div className="flex-1 overflow-hidden">
             <TextBox>Free, open, simple</TextBox>
             <TextBox className="mt-16 md:mt-10">Powerful tooling</TextBox>
           </div>
--- app/scenes/Phones.jsx
+++ app/scenes/Phones.jsx
@@ -6,22 +6,22 @@ import bgPatternCircles from "../../public/images/bg-pattern-circles.svg";
 
 const Phones = () => {
   return (
-    <div className="relative rounded-bl-[96px] rounded-tr-[96px] bg-gradient-to-r from-[#2c2d3f] to-[#3f4164] py-28 xl:py-20 lg:py-16">
+    <div className="relative  rounded-bl-[96px] rounded-tr-[96px] bg-gradient-to-r from-[#2c2d3f] to-[#3f4164] py-28 xl:py-20 lg:py-16">
       <div className="absolute left-0 top-0 h-full w-full overflow-hidden rounded-bl-[96px] rounded-tr-[96px] ">
-        <Image
+        {/* <Image
           alt="bg pattern circles"
           src={bgPatternCircles}
           className="absolute -bottom-[20%] -left-[10%] scale-110 xl:-left-[20%] xl:scale-100 sm:bottom-0 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-40 sm:scale-150 xs:-translate-y-56"
-        />
+        /> */}
       </div>
       <Container className="relative sm:text-center">
         <div className="flex lg:gap-8 md:gap-16 sm:flex-col xs:gap-4">
           <div className="flex flex-1 items-center justify-center ">
-            <Image
+            {/* <Image
               alt="illustration phones"
               src={illustrationPhones}
               className="h-auto w-[150px] translate-y-5  scale-[3.5]  xl:scale-[3] lg:translate-y-4 lg:scale-[2.5] sm:w-[93px] sm:-translate-y-20 sm:scale-[4] xs:scale-[3.1]"
-            />
+            /> */}
           </div>
           <PhonesDesc />
         </div>
