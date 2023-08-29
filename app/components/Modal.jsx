fa8a168bcc38273996334ccb26a8c7f9f88c90d4
Dimas hilmawan <hilmawan22@gmail.com>
2023-08-29 22:56:29 +0700

    fini

--- app/components/Modal.jsx
+++ app/components/Modal.jsx
@@ -19,7 +19,7 @@ const Modal = ({ onCloseModal, onToggleDropdown, dropdown }) => {
       initial={{ opacity: 0, scale: 0.3 }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0 }}
-      className={`relative  z-[200] mx-auto w-[48rem] max-w-3xl rounded-2xl bg-white/95 px-16 pb-12 pt-8 text-xl text-[rgba(31,63,91,0.8)] shadow-xl lg:w-[calc(100%_-_8rem)] md:w-[calc(100%_-_6rem)] md:px-12 sm:w-[calc(100%_-_4rem)] sm:px-8 sm:text-lg xs:w-[calc(100%_-_2rem)] xs:px-4 xs:pb-8 xs:pt-4 xs:text-base`}
+      className={`relative  z-[200] mx-auto w-[48rem] max-w-3xl  bg-white/95 px-16 pb-12 pt-8 text-xl text-[rgba(31,63,91,0.8)] lg:w-[calc(100%_-_8rem)] md:w-[calc(100%_-_6rem)] md:px-12 sm:w-[calc(100%_-_4rem)] sm:px-8 sm:text-lg xs:w-[calc(100%_-_2rem)] xs:px-4 xs:pb-8 xs:pt-4 xs:text-base`}
     >
       <button
         className="absolute right-2 top-2 mx-auto flex h-10 w-10 items-center justify-center rounded-full sm:-bottom-20 sm:h-12 sm:w-12"
