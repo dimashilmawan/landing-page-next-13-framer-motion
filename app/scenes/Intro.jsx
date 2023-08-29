b082a0b8b553022f68ede5ca525964c3ddb673cd
Dimas hilmawan <hilmawan22@gmail.com>
2023-08-30 02:36:29 +0700

    final fix

--- app/scenes/Intro.jsx
+++ app/scenes/Intro.jsx
@@ -2,8 +2,8 @@ import Container from "@/components/Container";
 import IntroContent from "@/components/intro/IntroContent";
 import Image from "next/image";
 
-import bgPatternIntroDesk from "@/images/bg-pattern-intro-desktop.png";
-import bgPatternIntroMobile from "@/images/bg-pattern-intro-mobile.png";
+import bgPatternIntroDesk from "@/images/bg-pattern-intro-desktop.svg";
+import bgPatternIntroMobile from "@/images/bg-pattern-intro-mobile.svg";
 
 const Intro = () => {
   return (
@@ -12,15 +12,13 @@ const Intro = () => {
         <Image
           alt="background pattern intro desk"
           src={bgPatternIntroDesk}
-          priority
-          placeholder="blur"
+          // priority
           className=" h-auto w-full -translate-y-16 translate-x-[28rem] scale-[1.75] object-cover xl:translate-x-[24rem] lg:-translate-y-8 lg:translate-x-[16rem]  md:translate-x-[12rem] sm:hidden "
         />
         <Image
           alt="background pattern intro mobile"
           src={bgPatternIntroMobile}
-          priority
-          placeholder="blur"
+          // priority
           className="hidden h-auto w-full translate-x-1/3 translate-y-24 scale-[3.25] object-cover sm:block"
         />
       </div>
